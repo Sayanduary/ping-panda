c228ef3bb4ad81401d8cf541225021da6cadc1d6
Sayan Duary <sayanduary@gmail.com>
2026-07-04 11:07:52 +0530

    feat: integrate Prisma and update dependencies
    - Added Prisma client and configuration for PostgreSQL database.
    - Created a new Prisma schema for the Post model with basic fields.
    - Introduced MaxWidthWrapper component for layout consistency.
    - Updated page structure in Home component to utilize new layout.
    - Removed old global styles and replaced with Tailwind CSS imports.
    - Added utility function for class name merging using clsx and tailwind-merge.

---
+++ src/app/components/MaxWidthWrapper.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import { cn } from "@/utils";
+
+interface MaxWidthWrapperProps {
+  className?: string;
+  children: ReactNode;
+}
+
+export const MaxWidthWrapper = ({
+  className,
+  children,
+}: MaxWidthWrapperProps) => {
+  <div className={cn()}></div>;
+};
